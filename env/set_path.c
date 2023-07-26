3e77770ee77a1e5094c1da91a6f4a551b521a97a
anurieroglu <anurieroglu1@gmail.com>
2023-07-26 18:13:48 +0300

    setting to path

---
+++ env/set_path.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeroglu <aeroglu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 17:27:15 by aeroglu           #+#    #+#             */
+/*   Updated: 2023/07/26 17:36:03 by aeroglu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	set_paths(void)
+{
+	char	*path;
+
+	if (g_ms.paths)
+		free_array(g_ms.paths);
+	path = get_env("PATH");
+	if (!(*path))
+		g_ms.paths = NULL;
+	else
+		g_ms.paths = ft_split(path, ':');
+	free(path);
+}
