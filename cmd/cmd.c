fc849c9e2a40e0d57cd720818776b6325e18f5fd
anurieroglu <anurieroglu1@gmail.com>
2023-07-27 21:23:21 +0300

    fixed cmd starting

--- cmd/cmd.c
+++ cmd/cmd.c
@@ -6,21 +6,64 @@
 /*   By: aeroglu <aeroglu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 20:40:57 by aeroglu           #+#    #+#             */
-/*   Updated: 2023/07/26 20:46:14 by aeroglu          ###   ########.fr       */
+/*   Updated: 2023/07/27 17:37:55 by aeroglu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	get_builtin(t_process *process)
+{
+	int	in;
+	int	out;
+
+	in = dup(0);
+	out = dup(1);
+	get_all_inputs(process);
+	set_all_outputs(process);
+	//ft_builtin()
+	dup2(in, 0);
+	dup2(out, 1);
+	close(in);
+	close(out);
+}
+
+void	wait_cmd(void)
+{
+	t_process	*process;
+
+	process = g_ms.process;
+	close_all_fd();
+	while (process)
+	{
+		if(process->pid != -1)
+		{
+			waitpid(process->pid, &errno, 0);
+			errno = WEXITSTATUS(errno);
+		}
+		process = process->next;
+	}
+}
+
 void	start_cmd(void)
 {
-	t_pipes	*process;
+	t_process	*process;
 
 	process = g_ms.process;
 	if (!process)
 		return ;
 	fill_all_heredoc();
 	if (g_ms.ignore)
-		return (close_all)
-
+		return (close_all_fd());
+	if (is_builtin(process->execute[0]) && g_ms.process_count == 1)
+	{
+		get_builtin(process);
+		process = process->next;
+	}
+	while (process)
+	{
+		run_cmd(process);
+		process = process->next;
+	}
+	wait_cmd();
 }
