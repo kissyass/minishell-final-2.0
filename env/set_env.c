29ac659f9fd7935054b15576c6c3541582fc7bac
anurieroglu <anurieroglu1@gmail.com>
2023-07-27 21:23:33 +0300

    setting enviroment

--- env/set_env.c
+++ env/set_env.c
@@ -5,7 +5,7 @@ char **ft_set_env(char **envp, int size)
     char **env;
     int i;
 
-    env = malloc(sizeof(char *) * (size + 1)); // 
+    env = malloc(sizeof(char *) * (size + 1)); //
     if (!env)
         return (NULL);
     i = -1;
@@ -15,4 +15,34 @@ char **ft_set_env(char **envp, int size)
     while (++i < size)
         env[i] = NULL;
     return(env);
+}
+
+char    *split_env(char *str)
+{
+    while (*str != '=')
+        str++;
+    str++;
+    return (ft_strdup(str));
+}
+
+char    *get_env(char *str)
+{
+    size_t  len;
+    char    **env;
+    char    *new_str;
+
+    env = g_ms.env;
+    new_str = ft_strjoin(str, "=");
+    len = ft_strlen(new_str);
+    while (*env)
+    {
+        if (!ft_strncmp(*env, new_str, len))
+        {
+            free(new_str);
+            return (split_env(*env));
+        }
+        env++;
+    }
+    free(new_str);
+    return (ft_calloc(sizeof(char *), 1));
 }
